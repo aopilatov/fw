a86f4c8f0910c0e66be1e83ff34765b3072cd066
saxik <saxopilatov@me.com>
2025-06-28 14:33:50 +0300

    pubsub update

--- packages/pubsub/src/pubsub.ts
+++ packages/pubsub/src/pubsub.ts
@@ -66,11 +66,11 @@ export class PubSub {
 
 	public getTopic<T extends keyof PubSubSchema>(id: T) {
 		const topic = this.topics[id];
-		if (!topic) throw new PubSubError(`Topic '${id}' is not registered`);
+		if (!topic && !this.isTest) throw new PubSubError(`Topic '${id}' is not registered`);
 
 		return {
 			publish: async (data: PubSubSchema[T]) => {
-				if (!this.client) return;
+				if (this.isTest) return;
 
 				const dataBuffer = Buffer.from(JSON.stringify(data));
 				if (process.env?.NODE_ENV === 'test') {
@@ -79,9 +79,9 @@ export class PubSub {
 				}
 
 				try {
-					await topic.topic.publishMessage({ data: dataBuffer });
+					await topic!.topic.publishMessage({ data: dataBuffer });
 				} catch (error) {
-					getLogger().error(`Failed to publish to topic '${id}' message`, topic.topic.getMetadata(), error);
+					getLogger().error(`Failed to publish to topic '${id}' message`, topic!.topic.getMetadata(), error);
 					throw new PubSubError(`Failed to publish to topic '${id}' message ${data?.toString()}`);
 				}
 			},
@@ -98,18 +98,18 @@ export class PubSub {
 					};
 				}
 
-				if (topic.subscriptions.has(name)) {
-					getLogger().warn(`Subscription ${name} already exists `, await topic.topic.getMetadata());
+				if (topic!.subscriptions.has(name)) {
+					getLogger().warn(`Subscription ${name} already exists `, await topic!.topic.getMetadata());
 					throw new PubSubError(`Subscription ${name} already exists`);
 				}
 
-				if (this.isEmulator && !(await topic.topic.subscription(name).exists())[0]) {
-					getLogger().warn(`Subscription ${name} does not exist. Creating...`, await topic.topic.getMetadata());
-					await topic.topic.subscription(name).create(limitConfig ? { flowControl: limitConfig } : undefined);
+				if (this.isEmulator && !(await topic!.topic.subscription(name).exists())[0]) {
+					getLogger().warn(`Subscription ${name} does not exist. Creating...`, await topic!.topic.getMetadata());
+					await topic!.topic.subscription(name).create(limitConfig ? { flowControl: limitConfig } : undefined);
 				}
 
-				topic.subscriptions.set(name, {
-					subscription: topic.topic.subscription(name, limitConfig ? { flowControl: limitConfig } : undefined),
+				topic!.subscriptions.set(name, {
+					subscription: topic!.topic.subscription(name, limitConfig ? { flowControl: limitConfig } : undefined),
 					callback: async (message) => {
 						try {
 							const data: PubSubSchema[T] = JSON.parse(Buffer.from(message.data).toString());
@@ -127,16 +127,16 @@ export class PubSub {
 					},
 				});
 
-				getLogger().info(`Subscribed to subscription ${name}`, await topic.topic.getMetadata());
+				getLogger().info(`Subscribed to subscription ${name}`, await topic!.topic.getMetadata());
 
-				const subscription = topic.subscriptions.get(name)!;
+				const subscription = topic!.subscriptions.get(name)!;
 				subscription.subscription.on('message', subscription.callback);
 
 				return {
 					unsubscribe: async () => {
 						subscription.subscription.removeListener('message', subscription.callback);
-						getLogger().info(`Unsubscribed from subscription ${name}`, topic.topic.getMetadata());
-						topic.subscriptions.delete(name);
+						getLogger().info(`Unsubscribed from subscription ${name}`, topic!.topic.getMetadata());
+						topic!.subscriptions.delete(name);
 					},
 				};
 			},
