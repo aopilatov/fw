d155f65feead1c2d80c5190ee37bb271daf8f739
saxik <saxopilatov@me.com>
2025-10-21 12:29:10 +0300

    wip: pubsub

---
+++ packages/gcp-pubsub/src/index.ts
@@ -0,0 +1,4 @@
+export * from './errors';
+export * from './types';
+export * from './inject';
+export * from './gcpPubSub';
---
+++ packages/gcp-pubsub/src/inject.ts
@@ -0,0 +1,37 @@
+import { DIErrorInject, resolveToTypeWrapper, Constructable, Func, Registry } from '@fw/common';
+
+export function UseGcpPubSub(): Func {
+	return function (target: object, propertyName: string | symbol, index?: number): void {
+		const typeWrapper = resolveToTypeWrapper(undefined, target, propertyName, index);
+		if (
+			typeWrapper === undefined ||
+			typeWrapper.eagerType === undefined ||
+			typeWrapper.eagerType === Object ||
+			typeWrapper.eagerType?.['name'] !== 'GcpPubSub'
+		) {
+			throw new DIErrorInject(`${(target as Constructable<unknown>).constructor.name} -> ${propertyName.toString()}`);
+		}
+
+		Registry.registerHandler({
+			object: target as Constructable<unknown>,
+			propertyName: propertyName as string,
+			index: index,
+			value: (containerInstance) => {
+				const targetService = Registry.getService(target.constructor);
+				if (targetService?.instanceOf && !['action'].includes(targetService.instanceOf)) {
+					throw new DIErrorInject(
+						`${(target as Constructable<unknown>).constructor.name} -> ${propertyName.toString()} -> GcpPubSub is allowed only in actions`,
+					);
+				}
+
+				const evaluatedLazyType = typeWrapper.lazyType();
+
+				if (evaluatedLazyType === undefined || evaluatedLazyType === Object) {
+					throw new DIErrorInject(`${(target as Constructable<unknown>).constructor.name} -> ${propertyName.toString()}`);
+				}
+
+				return Registry.get(containerInstance, 'system', evaluatedLazyType);
+			},
+		});
+	};
+}
